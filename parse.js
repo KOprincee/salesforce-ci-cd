0671aba16e9b158a683fa07e4248331afad5cc4b
Prince Kothari <princemkothari@gmail.com>
2024-05-15 22:29:04 +0530

    commit

---
+++ parse.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+async function extractTests(){
+    const runTestsString = '';
+    fs.readFile(__dirname + '/build.xml', (err, data) => {
+        if (err) {
+            console.error('Error reading XML file:', err);
+            return;
+        }
+    
+        // Parse the XML data
+        xml2js.parseString(data, (err, result) => {
+            if (err) {
+                console.error('Error parsing XML:', err);
+                return;
+            }
+    
+            // Extract the values of runTest tags
+            const runTests = result.TestSuite.runTest;
+            runTestsString = runTests.join(', ');  // Combine values into a single string
+
+            console.log(runTestsString);
+        });
+    });
+
+    let testsFile = __dirname+'/testsToRun.txt';
+    await fs.promises.writeFile(testsFile,'all');
+
+    await fs.promises.writeFile(testsFile,tests);
+    await fs.promises.appendFile(testsFile,'\n');
+}
+
+extractTests();
