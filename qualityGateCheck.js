c61eaa02fa894d9df553988655e97d4029cbe4eb
Prince Kothari <81864518+KOprincee@users.noreply.github.com>
2024-05-17 20:31:11 +0530

    Update qualityGateCheck.js

--- qualityGateCheck.js
+++ qualityGateCheck.js
@@ -1,59 +1,46 @@
 const fs = require("fs");
+const outputFilePath = "./testResults/codeCoverageReadableFile.txt";
 
-// Path to the text file that contains the JSON file name
-const filenamePath = "./test-run-id.txt";
+filename = "./testResults/test-result.json";
 
-// Path to the output text file where the result will be stored
-const outputFilePath = "./codeCoverageReadableFile.txt";
-
-fs.readFile(filenamePath, "utf8", (err, filename) => {
+// Read the JSON file
+fs.readFile(filename, "utf8", (err, data) => {
   if (err) {
-    console.error("Error reading the filename file:", err);
+    console.error("Error reading the JSON file:", err);
     return;
   }
 
-  // Remove any trailing newlines or spaces
-  filename = "test-result-" + filename.trim() + ".json";
+  try {
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
 
-  // Read the JSON file
-  fs.readFile(filename, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading the JSON file:", err);
-      return;
+    // Extract coveredPercent values from coverage.coverage array
+    const coveredPercents = jsonData.coverage.coverage.map(
+      (item) => item.coveredPercent + ";" + item.name,
+    );
+
+    let result = "";
+    for (const item of coveredPercents) {
+      const [percentageStr, className] = item.split(";");
+      const percentage = parseInt(percentageStr, 10);
+      if (percentage < 85) {
+        result += `Test Class ${className} has test coverage of ${percentage} which is less than specified Quality Gate.\n`;
+      }
     }
 
-    try {
-      // Parse the JSON data
-      const jsonData = JSON.parse(data);
-
-      // Extract coveredPercent values from coverage.coverage array
-      const coveredPercents = jsonData.coverage.coverage.map(
-        (item) => item.coveredPercent + ";" + item.name,
-      );
-
-      let result = "";
-      for (const item of coveredPercents) {
-        const [percentageStr, className] = item.split(";");
-        const percentage = parseInt(percentageStr, 10);
-        if (percentage < 85) {
-          result += `Test Class ${className} has test coverage of ${percentage} which is less than specified Quality Gate.\n`;
-        }
-      }
-
-      if (result.indexOf("Test Class") == -1) {
-        result = "true";
-      }
-
-      // Write the result to the output file
-      fs.writeFile(outputFilePath, result.trim(), "utf8", (err) => {
-        if (err) {
-          console.error("Error writing the result to the file:", err);
-        } else {
-          console.log(result.trim());
-        }
-      });
-    } catch (parseErr) {
-      console.error("Error parsing JSON data:", parseErr);
+    if (result.indexOf("Test Class") == -1) {
+      result = "true";
     }
-  });
+
+    // Write the result to the output file
+    fs.writeFile(outputFilePath, result.trim(), "utf8", (err) => {
+      if (err) {
+        console.error("Error writing the result to the file:", err);
+      } else {
+        console.log(result.trim());
+      }
+    });
+  } catch (parseErr) {
+    console.error("Error parsing JSON data:", parseErr);
+  }
 });
